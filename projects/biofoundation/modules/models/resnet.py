f5336e98bde2df0002fae1c4affd945a18f0f440
Tom Hauschild <stu216509@mail.uni-kiel.de>
2025-04-29 12:35:30 +0000

    added resnet

--- projects/biofoundation/modules/models/resnet.py
+++ projects/biofoundation/modules/models/resnet.py
@@ -1,28 +1,22 @@
-from biofoundation.modules.models.birdset_model import BirdSetModel
-from torch import nn
-import torchvision.models as models
 import torch
+import torch.nn as nn
+import torchvision.models as models
 import torch.nn.functional as F
 from torchaudio.compliance import kaldi
+from biofoundation.modules.models.birdset_model import BirdSetModel
 
 
-class ViTModel(BirdSetModel):
-    """
-    ViT-B-16 model implemented like in the iNaturalist paper: https://openreview.net/pdf?id=QCY01LvyKm.
-    """
-
-    EMBEDDING_SIZE = 768
-
+class ResNet50(BirdSetModel):
     def __init__(
         self,
         num_classes: int,
-        embedding_size: int = EMBEDDING_SIZE,
+        embedding_size: int = 1000,
         local_checkpoint: str = None,
         load_classifier_checkpoint: bool = True,
         freeze_backbone: bool = False,
         preprocess_in_model: bool = True,
         classifier: nn.Module = None,
-    ) -> None:
+    ):
         super().__init__(
             num_classes=num_classes,
             embedding_size=embedding_size,
@@ -31,46 +25,32 @@ class ViTModel(BirdSetModel):
             preprocess_in_model=preprocess_in_model,
         )
 
-        self.num_classes = num_classes
         self.model = None
         self.load_model()
 
         if classifier is None:
-            self.model.heads.head = nn.Linear(embedding_size, num_classes)
+            self.classifier = nn.Linear(embedding_size, num_classes)
         else:
-            self.model.heads.head = classifier
+            self.classifier = classifier
 
         if local_checkpoint:
             self._load_local_checkpoint()
 
     def load_model(self) -> None:
-        self.model = models.vit_b_16(weights=None)
+        self.model = models.resnet50(weights=None)
 
-        checkpoint_path = "/workspace/models/vit/vit_single_mixup.pt"
+        checkpoint_path = "/workspace/models/resnet/r50_single_mixup.pt"
         state_dict = torch.load(checkpoint_path)
-
-        keys_to_remove = [key for key in state_dict.keys() if "heads.head" in key]
-        for key in keys_to_remove:
-            del state_dict[key]
+        state_dict.pop("fc.weight", None)
+        state_dict.pop("fc.bias", None)
 
         self.model.load_state_dict(state_dict, strict=False)
 
     def forward(
         self, input_values: torch.Tensor, labels: torch.Tensor | None
     ) -> torch.Tensor:
-        """
-        Forward pass through the model.
-
-        Args:
-            input_values (torch.Tensor): The input tensor for the classifier.
-            labels (Optional[torch.Tensor]): The true labels for the input values. Default is None.
-
-        Returns:
-            torch.Tensor: The output of the classifier.
-        """
-        # input_values = self.duplicate_channels(input_values)
         spectograms = self.preprocess(input_values)
-        return self.model(spectograms)
+        return self.classifier(self.model(spectograms))
 
     def preprocess(self, input_values: torch.Tensor) -> torch.Tensor:
 
