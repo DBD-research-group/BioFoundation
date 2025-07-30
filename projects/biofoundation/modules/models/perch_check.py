ac6d363e7840a2e1372a4f793d962103f852a6ef
Raphael Schwinger <raphaelschwinger@gmail.com>
2025-07-30 13:09:17 +0000

    check for missing lables in perch

---
+++ projects/biofoundation/modules/models/perch_check.py
@@ -0,0 +1,96 @@
+BIRD_NAMES_HIGH_SIERRAS = [
+    'gcrfin', 'whcspa', 'amepip', 'sposan', 'rocwre', 'brebla', 'daejun', 'foxspa',
+    'clanut', 'moublu', 'casfin', 'mallar3', 'herthr', 'amerob', 'yerwar', 'yelwar',
+    'dusfly', 'mouchi', 'orcwar', 'warvir', 'norfli']
+
+BIRD_NAMES_COLUMBIA_COSTA_RICA = [
+    "littin1", "grhcha1", "colcha1", "compau", "compot1", "buvhum1", "stvhum2",
+    "rtlhum",
+    "andeme1", "squcuc1", "rebpig1", "whtdov", "gycwor1", "roahaw", "gryhaw2", "trsowl",
+    "rinkin1", "bucmot2", "higmot1", "rehbar1", "kebtou1", "acowoo", "hofwoo1", "linwoo1",
+    "yehcar1", "laufal1", "orcpar", "whcpar", "brwpar1", "whfpar1", "orfpar", "crfpar",
+    "oliwoo1", "sthwoo1", "azaspi1", "yebela1", "goftyr1", "cotfly1", "wewpew", "rumfly1",
+    "socfly1", "grekis", "bobfly1", "trokin", "ducfly", "lotman1", "mastit1", "rotbec",
+    "yetvir", "blcjay1", "grnjay", "brnjay", "melwar1", "runwre1", "whiwre1", "rubwre1",
+    "rawwre1", "plawre1", "houwre", "andsol1", "clcrob", "yeceup1", "yeteup1", "ccbfin",
+    "rucspa1", "wegspa1", "pregrs2", "ruboro1", "monoro1", "yebori1", "melbla1", "grtgra",
+    "tenwar", "yelwar", "rucwar1", "rucwar", "sltred", "rcatan1", "strsal1", "butsal1",
+    "blhsal1", "banana", "yefgra1", "flrtan1", "sibtan2", "bugtan", "blctan1", "scrtan1",
+    "obnthr1"]
+
+BIRD_NAMES_AMAZON_BASIN = [
+    "gretin1", "cintin1", "littin1", "undtin1", "bartin2", "horscr1", "specha3",
+    "spigua1",
+    "stwqua1", "compot1", "strcuc1", "squcuc1", "scapig2", "pavpig2", "plupig2", "rudpig",
+    "blgdov1", "ruqdov", "grfdov1", "gycwor1", "greibi1", "amapyo1", "fepowl", "tabsco1",
+    "blttro1", "gnbtro1", "viotro3", "blctro1", "coltro1", "garkin1", "rinkin1", "bucmot4",
+    "blfjac1", "wespuf1", "blfnun1", "gilbar1", "letbar1", "whttou1", "yetwoo2", "litwoo2",
+    "whtwoo2", "gogwoo1", "scbwoo5", "rinwoo1", "renwoo1", "blacar1", "baffal1", "coffal1",
+    "buffal1", "cowpar1", "blhpar1", "meapar", "duhpar", "rebmac2", "oliwoo1", "plbwoo1",
+    "citwoo1", "lobwoo1", "amabaw1", "strwoo2", "elewoo1", "butwoo1", "stbwoo2", "strxen1",
+    "crfgle1", "chwfog1", "btfgle1", "spwant2", "bltant2", "pygant1", "whfant2", "lowant1",
+    "gryant1", "pltant1", "dutant2", "barant1", "plwant1", "fasant1", "greant1", "bsbeye1",
+    "gryant2", "pluant1", "goeant1", "rucant2", "blfant1", "rufant3", "astgna1", "wibpip1",
+    "yectyr1", "forela1", "whltyr1", "rinant2", "whbtot1", "ruftof1", "yemfly1", "gycfly1",
+    "gocspa1", "whcspa1", "eulfly1", "pirfly1", "grcfly1", "bobfly1", "gramou1", "whrsir1",
+    "ducfly", "ducatt1", "brratt1", "putfru1", "scrpih1", "blfcot1", "batman1", "royfly1",
+    "cinmou1", "whwbec1", "blcbec1", "ducgre1", "thlwre1", "bubwre1", "muswre2", "blbthr1",
+    "whnrob1", "hauthr1", "puteup1", "ruboro1", "olioro1", "sobcac1", "yercac1", "rcatan1",
+    "bubgro2", "grasal3", "butsal1", "partan1"]
+
+BIRD_NAMES_POWDERMILL_NATURE = [
+    "cangoo", "wiltur", "yebcuc", "amgplo", "reshaw", "rebwoo", "dowwoo",
+    "haiwoo",
+    "norfli", "pilwoo", "eawpew", "reevir1", "buhvir", "blujay", "amecro", "comrav",
+    "cedwax", "tuftit", "bkcchi", "ruckin", "carwre", "buggna", "whbnut", "brncre",
+    "woothr", "amerob", "eastow", "balori", "rewbla", "bnhcow", "ovenbi1", "louwat",
+    "buwwar", "bawwar", "naswar", "kenwar", "comyel", "hoowar", "amered", "babwar",
+    "chswar", "btnwar", "scatan", "robgro", "norcar", "swathr", "herthr", "veery"]
+
+BIRD_NAMES_SIERRA_NEVADA = [
+    "cangoo", "mouqua", "annhum", "batpig1", "moudov", "acowoo", "wilsap", "whhwoo",
+    "norfli", "olsfly", "wewpew", "hamfly", "dusfly", "pasfly", "warvir", "hutvir",
+    "casvir", "stejay", "comrav", "chbchi", "mouchi", "ruckin", "gockin", "bewwre",
+    "houwre", "rebnut", "brncre", "towsol", "amerob", "purfin", "casfin", "redcro",
+    "amegfi", "pinsis", "foxspa", "daejun", "whcspa", "vesspa", "linspa", "gnttow",
+    "spotow", "bnhcow", "orcwar", "naswar", "macwar", "yerwar", "btywar", "towwar",
+    "herwar", "wlswar", "westan", "bkhgro", "lazbun", "swathr", "herthr", "evegro"]
+
+BIRD_NAMES_SAPSUCKER = [
+    "cangoo", "snogoo", "tunswa", "wooduc", "mallar3", "moudov", "killde", "amewoo",
+    "solsan", "ribgul", "bcnher", "grbher3", "coohaw", "brdowl", "belkin1", "rebwoo",
+    "yebsap", "dowwoo", "haiwoo", "norfli", "pilwoo", "easpho", "eawpew", "aldfly",
+    "easkin", "grcfly", "reevir1", "warvir", "yetvir", "buhvir", "blujay", "amecro",
+    "comrav", "cedwax", "tuftit", "bkcchi", "treswa", "ruckin", "gockin", "houwre",
+    "whbnut", "rebnut", "brncre", "grycat", "eursta", "easblu", "woothr", "amerob",
+    "purfin", "houfin", "redcro", "amegfi", "pinsis", "daejun", "whtspa", "sonspa",
+    "swaspa", "eastow", "boboli", "balori", "rewbla", "bnhcow", "rusbla", "comgra",
+    "ovenbi1", "norwat", "buwwar", "naswar", "comyel", "hoowar", "amered", "bkbwar",
+    "yelwar", "chswar", "yerwar", "btnwar", "scatan", "robgro", "norcar", "herthr", "veery"]
+
+BIRD_NAMES_HAWAII = [
+    "hawgoo", "calqua", "wiltur", "kalphe", "blkfra", "chukar", "ercfra", "barpet",
+    "hawpet1", "hawhaw", "elepai", "skylar", "jabwar", "warwhe1", "omao", "comwax",
+    "palila", "iiwi", "apapan", "hawcre", "akepa1", "hawama", "houfin", "yefcan",
+    "norcar", "reblei", "melthr"]
+
+BIRD_NAMES_NIPS4BPLUS = [
+    "eurwry", "easwar1", "darwar1", "eurjay1", "eurnut2", "sonthr1", "coatit2",
+    "shttre1",
+    "gretit1", "eurlin1", "martit2", "comchi1", "grswoo", "lottit1", "combuz1", "eurrob1",
+    "eurmag1", "eurgre1", "houspa", "winwre4", "grywag", "firecr1", "woolar1", "blackc1",
+    "crelar1", "cirbun1", "trepip", "rocpet1", "comnig1", "eugori2", "eurbul",
+    "dunnoc1", "skylar", "thelar1", "comcha", "blutit", "barswa", "sarwar1", "eurbla",
+    "carcro1", "eurser1", "eurgol", "eutdov", "eucdov", "zitcis1", "cetwar1", "cowpig1",
+    "eursta", "spofly1", "redcro", "cretit2"]
+    
+missing = set([
+    'gnwtea', 'whwsco3', 'whwsco2', 'dusscr1', 'dulgua3', 'butpig1', 'cregui3', 'rinphe1', 'whieap1', 'shefra1', 'chnfra2', 'monnig1', 'webhum1', 'antman2', 'amtsun3', 'boorat1', 'rubrat1', 'wetsab1', 'gybsab5', 'stream2', 'eurcod2', 'whigrp1', 'yebfrd1', 'yebfrd2', 'spiimp2', 'mouimp1', 'whfplo2', 'soasni2', 'soasni3', 'silgul2', 'mewgul2', 'corshe', 'troshe5', 'litshe8', 'sacibi2', 'bkfibi1', 'y00839', 'recgos3', 'easmah1', 'augbuz2', 'morepo2', 'solboo1', 'solboo4', 'madsco1', 'afrsco3', 'sulsco5', 'spoeao2', 'fraeao1', 'indrol2', 'lickin2', 'bubkin2', 'varkin1', 'bubbee2', 'grnbee1', 'grnbee2', 'grnbee3', 'gowbar2', 'darbar3', 'y00400', 'blcwoo4', 'eugwoo2', 'gyhwoo4', 'bkrfla1', 'sousow1', 'y00678', 'levpar1', 'paplor3', 'elepit2', 'elepit6', 'ducwoo1', 'crbcan5', 'licspi5', 'necspi3', 'rubsti1', 'ruwant3', 'wesfie1', 'whbfie9', 'rufant5', 'rufant6', 'rufant7', 'chaant4', 'equant1', 'rufant8', 'chaant5', 'panant1', 'rufant9', 'oxaant1', 'rufant10', 'punant1', 'rufant11', 'whwtap1', 'partap4', 'blatap2', 'roltyr3', 'gryela3', 'y01036', 'moctyr7', 'olsfly2', 'yebfly3', 'tropew3', 'verfly', 'slbcht2', 'rufgra1', 'rusgra1', 'dusmyz4', 'ruffie2', 'satber1', 'blujeb1', 'capbat10', 'bkhbat2', 'weabat1', 'bluvan3', 'bkbtri2', 'cresht1', 'chgshr1', 'whbshb1', 'timori3', 'hacdro9', 'vemdro6', 'fotdro5', 'cstdro1', 'pelfan1', 'loemon1', 'blkmag2', 'bkhcan2', 'screup3', 'rutscr1', 'vissha1', 'grafly1', 'flojuf2', 'vivnil3', 'masyel2', 'sulwar3', 'malbrw1', 'sphlar12', 'klblar6', 'y00415', 'faclar8', 'dunlar5', 'barlar4', 'obfrob1', 'shtaka2', 'bocaka11', 'whbsho10', 'ryurob2', 'ryurob3', 'japrob2', 'narfly2', 'narfly3', 'sinbus6', 'crelar1', 'dunlar1', 'dunlar4', 'mstlar1', 'tstlar1', 'lesbri3', 'ochbul3', 'sunbul1', 'yelbul4', 'flabul3', 'gobwar3', 'rucwar4', 'heptan', 'scbcup3', 'bkfbun1', 'trogna2', 'bnhnut2', 'vibsta1', 'cbbwar4', 'talgrw1', 'cbbwar3', 'guathi2', 'bknwea2', 'viewea3', 'recwid3', 'sthmun2', 'quailf1', 'leswhi4', 'subwar6', 'easwar1', 'y00989', 'islthr24', 'gyhbul5', 'scrcac1', 'rolcis2', 'wincis2', 'delpri1', 'yebapa2', 'miowrw2', 'radacc2', 'auspip3', 'bkcbab3', 'capwhe8', 'gytwhe2', 'chwbab3', 'cobscb1', 'chbscb2', 'chbscb1', 'bkcbab1', 'pabill3', 'limwrb4', 'limwrb2', 'stcspa2', 'stcspa3', 'sabspa4', 'comhom2', 'norwhe3', 'rebwhe2', 'bkewhe1', 'mouwhe6', 'mouwhe7', 'retwhe3', 'orblea3', 'pltsun2', 'grnsun2', 'miosun3', 'grasal2', 'grasal4', 'whcsee2', 'barwaf1', 'slbcro4', 'paradi3', 'ashrob2', 'pacrob3', 'origre6'
+])
+all_bird_lists = [
+    BIRD_NAMES_HIGH_SIERRAS, BIRD_NAMES_COLUMBIA_COSTA_RICA, BIRD_NAMES_AMAZON_BASIN,
+    BIRD_NAMES_POWDERMILL_NATURE, BIRD_NAMES_SIERRA_NEVADA, BIRD_NAMES_SAPSUCKER,
+    BIRD_NAMES_HAWAII, BIRD_NAMES_NIPS4BPLUS
+]
+found = missing.intersection(set().union(*all_bird_lists))
+print(found)
